26f5816ef4ec7b7eef6c63424c58f0faab58f921
Feras-ALdemashki <feras.aldemashki@hotmail.com>
2025-08-08 12:42:00 +0200

    cleaning some files

--- classes/Player.js
+++ classes/Player.js
@@ -1,22 +1,6 @@
 import { GRAVITY } from "../constants.js";
 import { C } from "../constants.js";
 import { CANVAS } from "../constants.js";
-const fallRight = new Image();
-fallRight.src = "./images/player/Fall.png";
-const fallLeft = new Image();
-fallLeft.src = "./images/player/FallLeft.png";
-const idle = new Image();
-idle.src = "./images/player/Idle.png";
-const idleLeft = new Image();
-idleLeft.src = "./images/player/IdleLeft.png";
-const jump = new Image();
-jump.src = "./images/player/Jump.png";
-const jumpLeft = new Image();
-jumpLeft.src = "./images/player/JumpLeft.png";
-const runRight = new Image();
-runRight.src = "./images/player/Run.png";
-const runLeft = new Image();
-runLeft.src = "./images/player/RunLeft.png";
 
 export class Player {
   constructor(position) {
@@ -32,9 +16,9 @@ export class Player {
     C.beginPath();
     C.fillStyle = "red";
     C.arc(
-      this.position.x + this.width / 2, // center X
-      this.position.y + this.height / 2, // center Y
-      this.width / 2, // radius (you can also use Math.min(width, height)/2)
+      this.position.x + this.width / 2,
+      this.position.y + this.height / 2,
+      this.width / 2,
       0,
       Math.PI * 2
     );
